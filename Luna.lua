60af414746cce9e347263f9ea6d3a26a5f36e138
TheKing <164037583+Wakasax@users.noreply.github.com>
2025-04-17 23:07:01 -0300

    Update Luna.lua

--- Luna.lua
+++ Luna.lua
@@ -1,19 +1,12 @@
 if game.PlaceId == 4058282580 then
+    
+
 
     local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
     local Window = OrionLib:MakeWindow({Name = "Luna Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Kzinnx", IntroEnable = false})
 
-    --variavel
-    _G.AutoSell = false
 
-    --function
-    function AutoSell()
-        while AutoSell.value do
-            game:GetService("ReplicatedStorage").Events.SellRequest:FireServer()
-            task.wait(1)
-
-        end
 
     local Main = Window:MakeTab({
             Name = "Luna",
@@ -28,60 +21,10 @@ if game.PlaceId == 4058282580 then
         Name = "Auto-sell",
         Default = false,
         Callback = function(Value)
-            _G.AutoSell(value)
-            if value then
-                task.spawn(AutoSell)
-            end
+            game:GetService("ReplicatedStorage").Events.SellRequest:FireServer()  
+            wait(1)
 
         end    
     })
-
-
-
-    end
-
-
-
-
-
-
-end
-
-
-if game.PlaceId == 85896571713843 then -- bubble gum simulator inf
-
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-
-    local Window = OrionLib:MakeWindow({Name = "Luna Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Luna"})
-
-    local Main = Window:MakeTab({
-        Name = "Luna",
-        Icon = "rbxassetid://4483345998",
-        PremiumOnly = false
-    })
-
-    local Main = Tab:AddSection({
-        Name = "Auto-farm"
-    })
-
-    Main:AddToggle({
-        Name = "auto soprar",
-        Default = false,
-        Callback = function(Value)
-
-        end
-    })
-    Main:AddToggle({
-        Name = "auto vender",
-        Default = false,
-        Callback = function(Value)
-
-        end
-    })
-
-
-
-
-
 
 end

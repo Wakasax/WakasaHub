6b53c132202b82afe4e25e19cca72ae1764f9b26
TheKing <164037583+Wakasax@users.noreply.github.com>
2025-04-17 23:56:01 -0300

    Update Luna.lua

--- Luna.lua
+++ Luna.lua
@@ -1,29 +1,24 @@
 if game.PlaceId == 4058282580 then
-    
 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "thx Hubi", HidePremium = false, SaveConfig = true, ConfigFolder = "Devi thx", IntroEnable = false})
 
-    local Window = OrionLib:MakeWindow({Name = "Luna Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Kzinnx", IntroEnable = false})
-
-
-
-    local Main = Window:MakeTab({
-            Name = "Luna",
-            Icon = "rbxassetid://4483345998",
-            PremiumOnly = false
-    })
-    local Section = Main:AddSection({
-        Name = "Auto-farm"
-    })
-
-    Main:AddToggle({
-        Name = "Auto-sell",
-        Default = false,
-        pcall = function(Value)
-            game:GetService("ReplicatedStorage").Events.SellRequest:FireServer()  
+local Main = Window:MakeTab({
+    Name = "auto farmi",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+ 
+local Section = Main:AddSection({
+    Name = "a"
+})
+  
+   Main:AddToggle({
+    Name = "auto vende",
+    Default = false,
+    Callback = function(Value) game:GetService("ReplicatedStorage").Events.SellRequest:FireServer()  
             task.wait(0.1)
-
-        end    
-    })
+    end    
+})
 end

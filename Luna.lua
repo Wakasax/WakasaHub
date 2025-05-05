c6cf97003cc0f1481c9c4afb4338620a2868d940
TheKing <164037583+Wakasax@users.noreply.github.com>
2025-05-04 23:35:14 -0300

    Update Luna.lua

--- Luna.lua
+++ Luna.lua
@@ -1,4 +1,3 @@
-
 if game.PlaceId == 86782616351214 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
@@ -20,7 +19,7 @@ Main:AddToggle({
 	Default = false,
 	Callback = function(Value)
         wait(0.5)
-        
+
         game:GetService("ReplicatedStorage"):WaitForChild("\207\155\207\157\206\182\206\183\206\179\206\182\206\182\207\157\206\178"):WaitForChild("\208\147\208\168\208\130\208\131\208\130\208\148\208\168\208\146\208\148\208\150"):FireServer(unpack(args))
 	end    
 })

28b80da62475eb6374853de35b033b9ca63aaa63
TheKing <164037583+Wakasax@users.noreply.github.com>
2025-05-05 00:22:39 -0300

    Update Luna.lua

--- Luna.lua
+++ Luna.lua
@@ -1,32 +1,60 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
-    local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-    local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-    local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-    
-    
-    local Window = Fluent:CreateWindow({
-        Title = "Luna Hub",
-        SubTitle = "",
-        TabWidth = 100,
-        Size = UDim2.fromOffset(430, 300), --tamanho da UI
-        Acrylic = false,
-        Theme = "Amethyst",
-        MinimizeKey = Enum.KeyCode.LeftControl
-    })
 
-    local Tabs = {
-        Main = Window:AddTab({ Title = "• farm", Icon = "rbxassetid://18831448204" }),
-        Settings = Window:AddTab({ Title = "• Settings", Icon = "rbxassetid://18319394996" })
-    }
-    Window:SelectTab(1)
+local Window = Fluent:CreateWindow({
+    Title = "With ❤️ Nami Hub",
+    SubTitle = "",
+    TabWidth = 100,
+    Size = UDim2.fromOffset(430, 300), --tamanho da UI
+    Acrylic = false,
+    Theme = "Amethyst",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
 
-    local attack = Tabs.Main:AddToggle("Auto attack", {Title = "Auto attack", Default = false})
-    
-    attack:OnChanged(function()
-        while attack.Value do
-            --velocidade do remote em segundos
-            wait(1)
-            --remote
-            startAutoClick()
+--tabs
+local Tabs = {
+    Main = Window:AddTab({ Title = "• Infor", Icon = "rbxassetid://18831448204" }),
+    Settings = Window:AddTab({ Title = "• Settings", Icon = "rbxassetid://18319394996" })
+}
+Window:SelectTab(1)
+
+local autoClickEnabled = false
+
+
+local function startAutoClick()
+    task.spawn(function()
+        while autoClickEnabled do
+            pcall(function()
+                local vim = game:GetService("VirtualInputManager")
+                vim:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+                vim:SendMouseButtonEvent(0, 0, 0, false, game, 0)
+            end)
+            task.wait(0.1)
         end
     end)
+end
+
+--exemplo de toggle
+local attack = Tabs.Main:AddToggle("Auto attack", {Title = "Auto attack", Default = false})
+
+attack:OnChanged(function()
+    while attack.Value do
+        --velocidade do remote em segundos
+        wait(1)
+        --remote
+        startAutoClick()
+    end
+end)
+
+
+--settings do fluent
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
